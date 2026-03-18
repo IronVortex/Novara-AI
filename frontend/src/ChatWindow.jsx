0c0c193d9e71a986485e85d1615a3c87ccece61a
IronVortex <200262032+IronVortex@users.noreply.github.com>
2026-03-18 22:09:08 +0530

    creating ChatWindow

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,8 +1,21 @@
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx"
 function ChatWindow(){
     return(
-        <div>
-        ChatWindow
+        <div className="chatWindow"> 
+        <div className="navbar">
+            <span>NOVARA &nbsp;  AI &nbsp; <i class="fa-solid fa-angle-down"></i></span>
+            <div className="userIcondiv">
+                <i class="fa-solid fa-user"></i>
+            </div>
+        </div>
+        <Chat></Chat>
+        <div className="chatInput">
+            <div className="userInput">
+                 
+            </div>
+
+        </div>
         </div>
     )
 }
