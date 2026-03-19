ebc1317c54e2acba8fad35a5dfc19de06d620d7d
IronVortex <200262032+IronVortex@users.noreply.github.com>
2026-03-19 22:54:39 +0530

    styling ChatWindow

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -6,15 +6,21 @@ function ChatWindow(){
         <div className="navbar">
             <span>NOVARA-AI &nbsp; <i class="fa-solid fa-angle-down"></i></span>
             <div className="userIcondiv">
-                <i class="fa-solid fa-user"></i>
+               <span className="userIcon"><i class="fa-solid fa-user"></i></span> 
             </div>
         </div>
         <Chat></Chat>
         <div className="chatInput">
             <div className="userInput">
-                 
+                 <input placeholder="Ask Anything...">
+                 </input>
+                 <div id="submit">
+                    <i class="fa-solid fa-paper-plane"></i>
+                 </div>
             </div>
-
+            <p className="info">
+                Novara AI can make mistakes . Check important information . See cookie preferences .
+            </p>
         </div>
         </div>
     )
